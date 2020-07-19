39f46281111421e0179feb9e6e3538fdeb343ea1
cheminacci <65716931+cheminacci@users.noreply.github.com>
2020-07-19 10:31:08 -0400

    Add files via upload

---
+++ fixScroll.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+  
+synclient VertScrollDelta=-101           
+synclient HorizScrollDelta=-101          
+synclient VertEdgeScroll=1             
+synclient HorizEdgeScroll=1            
+synclient VertTwoFingerScroll=1             
+synclient HorizTwoFingerScroll=1             
+synclient MinSpeed=1             
+synclient MaxSpeed=1.75          
+synclient AccelFactor=0.0392234     
+synclient TouchpadOff=0             
+synclient RTCornerButton=0             
+synclient RBCornerButton=0             
+synclient LTCornerButton=0             
+synclient LBCornerButton=0             
+synclient TapButton1=1             
+synclient ClickFinger1=1             
+synclient ClickFinger2=0             
+synclient ClickFinger3=0             
+synclient ClickPad=0   
+synclient MaxDoubleTapTime=400   
